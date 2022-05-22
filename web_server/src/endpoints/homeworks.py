9f8caf615fd4e922f11e5c054f83b81a863b27ba
Екатерина Шелухина <shelukchina2447@gmail.com>
2022-05-22 16:25:13 +0300

    Add examle with params

--- web_server/src/endpoints/homeworks.py
+++ web_server/src/endpoints/homeworks.py
@@ -9,17 +9,17 @@ from database.src.database import Database
 from web_server.src.services import homeworks_service, submitions_service
 
 homework_router = APIRouter(prefix="/homeworks", tags=["Homeworks"])
-templates = Jinja2Templates(directory="../../../../interface")
+templates = Jinja2Templates(directory="interface")
 
 
 @homework_router.get('/', response_class=HTMLResponse)
 def get_all_homeworks(request: Request, db: Session = Depends(Database.get_db)):
     status_code, content = homeworks_service.get_all_homeworks(db)
-    return templates.TemplateResponse("index.html", {"request": request})
-    # return JSONResponse(status_code=status_code, content=jsonable_encoder(content))
-
-
-@homework_router.get('/{id}/submitions')
-def get_all_submitions_by_homework_id(id: int, db: Session = Depends(Database.get_db)):
-    status_code, content = submitions_service.get_all_submitions_by_homework_id(id, db)
     return JSONResponse(status_code=status_code, content=jsonable_encoder(content))
+
+
+@homework_router.get('/{id}/submitions', response_class=HTMLResponse)
+def get_all_submitions_by_homework_id(request: Request, id: int, db: Session = Depends(Database.get_db)):
+    status_code, content = submitions_service.get_all_submitions_by_homework_id(id, db)
+    # return JSONResponse(status_code=status_code, content=jsonable_encoder(content))
+    return templates.TemplateResponse("index.html", {"request": request, "id": id})
